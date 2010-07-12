c8268cc3a025e944d431a23a23c4bfc51cb2eaf8
mdakin <mdakin@e79309c6-3b30-11de-a81c-fde7d73ceb89>
2010-07-12 09:40:05 +0000

    POS class with all current primary POS types and label information.
    git-svn-id: http://zemberek3.googlecode.com/svn/trunk@50 e79309c6-3b30-11de-a81c-fde7d73ceb89

--- src/zemberek3/structure/POS.java
+++ src/zemberek3/structure/POS.java
@@ -1,5 +1,47 @@
 package zemberek3.structure;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public enum POS {
-    NOUN, VERB, ADJECTIVE, ADVERB
+    UNKNOWN("Unknown", "Unknown"), // Unknown POS
+    NOUN("Noun", "IS"), // TR: ad, isim
+    VERB("Verb", "FI"), // TR: fiil, eylem
+    ADJECTIVE("Adj", "SI"), // TR: sifat
+    ADVERB("Adv", "ZARF"), // TR: zarf
+    PRONOUN("Pron", "ZAM"), // TR: zamir
+    PROPER_NOUN("PropNoun", "OZ"), // TR: Ozel isim
+    TIME("Time", "ZA"), // TR: Zaman
+    QUESTION("SO", "SO"), // TR: soru
+    NUMERAL("Num", "NUM"), // TR: sayi
+    INTERJECTION("Interj", "UN"), // TR: unlem
+    PARTICLE("Particle", "EDAT"), // TR: edat
+    DUPILCATOR("Dup", "YA"), // TR: Yansima, Yanki, Ikileme
+    CONJUNCTION("Conj", "BAGLAC"); // TR: baglac
+
+    private String label;
+    private String zemberekAbbrev;
+
+    POS(String label, String zemberekAbbrev) {
+        this.label = label;
+        this.zemberekAbbrev = zemberekAbbrev;
+    }
+
+    private static final Map<String, POS> shortToPOS = new HashMap<String, POS>();
+    private static final Map<String, POS> zemberekToPOS = new HashMap<String, POS>();
+
+    static {
+        for (POS p : POS.values()) {
+            shortToPOS.put(p.label, p);
+            zemberekToPOS.put(p.zemberekAbbrev, p);
+        }
+    }
+
+    public static POS getPOSByLabel(String label) {
+        POS p = shortToPOS.get(label);
+        if (p != null)
+            return p;
+        return POS.UNKNOWN;
+    }
+
 }
