cc011c8d74db54f778ae955f450ac784b3a66283
ahmetaa <ahmetaa@e79309c6-3b30-11de-a81c-fde7d73ceb89>
2010-09-08 08:22:10 +0000

    wictionary related stuff
    git-svn-id: http://zemberek3.googlecode.com/svn/trunk@57 e79309c6-3b30-11de-a81c-fde7d73ceb89

---
+++ src-exp/extract/TurkishWictionaryExtractor.java
@@ -0,0 +1,138 @@
+package extract;
+
+import com.google.common.collect.Sets;
+import org.jcaki.SimpleTextWriter;
+import org.jcaki.Strings;
+import zemberek3.structure.POS;
+
+import javax.xml.stream.XMLInputFactory;
+import javax.xml.stream.XMLStreamConstants;
+import javax.xml.stream.XMLStreamException;
+import javax.xml.stream.XMLStreamReader;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.util.*;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class TurkishWictionaryExtractor {
+
+
+    static Set<String> typeSet = new HashSet<String>();
+
+    static Set<String> ignoreType = Sets.newHashSet("Önek", "Sonek", "Birleşik Sıfat", "Mecaz",
+            "Ad{{Deyimler}}", "Zarf}}", "Birleşik Fiil", "İsim|Türk==={{Söztürü|İsim", "Argo", "Matematik", "Kimya",
+            "Yardımcı eylem", "Ek Eylem", "Yanlış", "Çokluk", "Emir", "Deyim", "Simge", "Kız adı", "kadın-Erkek ismi",
+            "Soyadı", "Erkek adı", "Erkek ismi", "Kısaltma", "Belirteç", "Kız ismi", "Sayı", "Kısaltılmış sözcük",
+            "Ek", "Harf", "Özel isim", "Özel ad");
+
+    static Map<String, POS> posMap = new HashMap<String, POS>();
+
+    static {
+        posMap.put("Bağlaç", POS.CONJUNCTION);
+        posMap.put("Fiil", POS.VERB);
+        posMap.put("Eylem", POS.VERB);
+        posMap.put("Sıfat", POS.ADJECTIVE);
+        posMap.put("Zamir", POS.PRONOUN);
+        posMap.put("Ad", POS.NOUN);
+        posMap.put("İsim", POS.NOUN);
+        posMap.put("İlgeç", POS.PARTICLE);
+        posMap.put("Edat", POS.PARTICLE);
+        posMap.put("Zarf", POS.ADVERB);
+        posMap.put("Ünlem", POS.INTERJECTION);
+    }
+
+    public static void main(String[] args) throws XMLStreamException, IOException {
+        FileInputStream fileInputStream = new FileInputStream("/home/kodlab/Downloads/trwiktionary.xml");
+        XMLStreamReader staxXmlReader = XMLInputFactory.newInstance().createXMLStreamReader(fileInputStream);
+
+        String title = null;
+        SimpleTextWriter stw = SimpleTextWriter.keepOpenUTF8Writer(new File("wictionary-lemmas.txt"));
+        SimpleTextWriter compoundWriter = SimpleTextWriter.keepOpenUTF8Writer(new File("wictionary-compounds.txt"));
+        SimpleTextWriter properNounWriter = SimpleTextWriter.keepOpenUTF8Writer(new File("wictionary-proper-nouns.txt"));
+        for (int event = staxXmlReader.next(); event != XMLStreamConstants.END_DOCUMENT; event = staxXmlReader.next()) {
+            switch (event) {
+                case XMLStreamConstants.START_ELEMENT:
+                    String elName = staxXmlReader.getLocalName();
+                    if (elName.equals("title")) {
+                        title = staxXmlReader.getElementText();
+                    }
+
+                    if (elName.equals("text")) {
+                        String content = staxXmlReader.getElementText();
+                        List<WikiLemma> lemmas = getLemmas(title, content);
+                        for (WikiLemma lemma : lemmas) {
+
+                            if (lemma.lemma.contains(" "))
+                                compoundWriter.writeLine(lemma);
+                            else if (Character.isUpperCase(lemma.lemma.charAt(0))) {
+                                properNounWriter.writeLine(lemma);
+                            } else {
+                                if (accept(lemma)) {
+                                    stw.writeLine(lemma);
+                                }
+                            }
+                        }
+                    }
+
+                    break;
+                case XMLStreamConstants.END_ELEMENT:
+                    // System.out.println("End element " + staxXmlReader.getLocalName());
+                    break;
+                default:
+                    break;
+            }
+        }
+        stw.close();
+        compoundWriter.close();
+        properNounWriter.close();
+        staxXmlReader.close();
+
+        for (String s : typeSet) {
+            System.out.println("pos = " + s);
+        }
+
+    }
+
+    static boolean accept(WikiLemma lemma) {
+        return lemma.pos != POS.UNKNOWN &&
+                Character.isLowerCase(lemma.lemma.charAt(0)) &&
+                Strings.containsNone(lemma.lemma, "0123456789");
+    }
+
+    static Pattern pattern = Pattern.compile("(?:Söztürü\\|)(.+?)(?:\\|Türkçe)");
+
+    private static List<WikiLemma> getLemmas(String lemma, String content) {
+        List<WikiLemma> lemmas = new ArrayList<WikiLemma>();
+
+        Matcher m = pattern.matcher(content);
+        while (m.find()) {
+            String type = m.group(1);
+            if (type.contains("çekilmiş"))
+                continue;
+            lemmas.add(new WikiLemma(m.group(1), lemma));
+        }
+        return lemmas;
+    }
+
+    static class WikiLemma {
+        POS pos;
+        String lemma;
+        String wikiType;
+
+
+        WikiLemma(String wikiType, String lemma) {
+            if (posMap.containsKey(wikiType)) {
+                this.pos = posMap.get(wikiType);
+            } else
+                this.pos = POS.UNKNOWN;
+            this.lemma = lemma;
+            this.wikiType = wikiType;
+        }
+
+        public String toString() {
+            return lemma + " : " + wikiType;
+        }
+    }
+}
