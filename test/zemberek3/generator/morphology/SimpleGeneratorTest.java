140cdf1cd4828331c610763ae53e0db4baaae4c1
ahmetaa <ahmetaa@e79309c6-3b30-11de-a81c-fde7d73ceb89>
2011-04-15 14:21:47 +0000

    suffix Ids as enums. I will find a way to make Suffixes more dynamic using these.
    git-svn-id: http://zemberek3.googlecode.com/svn/trunk@190 e79309c6-3b30-11de-a81c-fde7d73ceb89

--- test/zemberek3/generator/morphology/SimpleGeneratorTest.java
+++ test/zemberek3/generator/morphology/SimpleGeneratorTest.java
@@ -20,7 +20,6 @@ import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
 
-
 public class SimpleGeneratorTest {
     @Test
     public void regenerateTest() throws IOException {
