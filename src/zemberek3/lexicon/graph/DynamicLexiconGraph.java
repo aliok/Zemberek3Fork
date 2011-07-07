628b03b152ae1625f8784e4d6616bc1b453573dc
ahmetaa <ahmetaa@e79309c6-3b30-11de-a81c-fde7d73ceb89>
2011-07-07 14:29:25 +0000

    ...
    git-svn-id: http://zemberek3.googlecode.com/svn/trunk@242 e79309c6-3b30-11de-a81c-fde7d73ceb89

--- src/zemberek3/lexicon/graph/DynamicLexiconGraph.java
+++ src/zemberek3/lexicon/graph/DynamicLexiconGraph.java
@@ -1,26 +1,33 @@
 package zemberek3.lexicon.graph;
 
 import zemberek3.lexicon.DictionaryItem;
+import zemberek3.lexicon.SuffixNodeGenerator;
 
-import java.util.List;
+import java.util.HashSet;
+import java.util.Set;
 
 public class DynamicLexiconGraph {
+
+    Set<SuffixNode> suffixRootNodes = new HashSet<SuffixNode>();
+    Set<StemNode> stemNodes = new HashSet<StemNode>();
     
+    StemNodeGenerator stemNodeGenerator = new StemNodeGenerator();
+    SuffixNodeGenerator suffixNodeGenerator = new SuffixNodeGenerator();
 
     public void addItem(DictionaryItem item) {
 
-
+        StemNode[] stems = stemNodeGenerator.generate(item);
+        for (StemNode stem : stems) {
+            if(!stemNodes.contains(stem)) {
+                stem.suffixRootNode = getSuffixRootNode(stem);
+            } else {
+                // duplicate stem!
+            }
+        }
     }
 
-
-    /**
-     * Generates StemNode objects from the dictionary item.
-     * <p>Most of the time a single StemNode is generated.
-     * @param item DictionaryItem
-     * @return one or more StemNode objects.
-     */
-    public void a(DictionaryItem item) {
-
+    public SuffixNode getSuffixRootNode(StemNode node) {
+        return null;
     }
 
 
